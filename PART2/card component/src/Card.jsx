1b7492ce7c1048431994f3193e219da65fcb9bcd
mshahilt <mshahilt3@gmail.com>
2024-11-05 10:14:51 +0530

    day 2

---
+++ PART2/card component/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="Profile picture" />
+            <h2 className="card-title">Shahil</h2>
+            <p className="card-text">I make websites</p>
+        </div>
+    )
+}
+
+export default Card;
---
+++ PART3_BUTTONS/button-component/src/App.jsx
@@ -0,0 +1,13 @@
+// HOW TO STYLE REACT COMPONENTS WITH CSS
+
+import Button from "./Button"
+function App() {
+
+  return (
+    <>
+      <Button></Button>
+    </>
+  )
+}
+
+export default App
